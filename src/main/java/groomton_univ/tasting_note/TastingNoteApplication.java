9cc879636f4341355e1021e12ee6e9a6bea9c3b3
ej9374 <109258160+ej9374@users.noreply.github.com>
2025-08-09 16:10:20 +0900

    초기세팅

---
+++ src/main/java/groomton_univ/tasting_note/TastingNoteApplication.java
@@ -0,0 +1,13 @@
+package groomton_univ.tasting_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastingNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastingNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/groomton_univ/tasting_note/TastingNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package groomton_univ.tasting_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastingNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
